ec0111d6a70fba7f66a7b664d26275d479b20f64
aidanm0322-del <aidanm0322@gmail.com>
2025-10-03 16:30:35 -0400

    Update asmiles1-assignment-5.py

--- asmiles1-assignment-5.py
+++ asmiles1-assignment-5.py
@@ -1,3 +1,5 @@
+#I used ai to help me debug the code and figure out why certian statements where printing the wrong information and also understand how to format the 
+
 # Challange 1:Number Sequence Generator
 current_number = int(input())
 print("=== Challenge 1: Collatz Conjecture ===")
@@ -10,7 +12,7 @@ while current_number != 1:
     if current_number % 2 == 0:
         current_number = current_number // 2
     else:
-        step_count = (step_count * 3) + 1  
+        current_number = (current_number*3) + 1  
     print(" ", end="")
     print(current_number, end="")
     step_count += 1
@@ -27,10 +29,10 @@ print("=== Challenge 2: Prime Number Checker ===")
 n =int(input())
 if n > 1:
     print(f"Enter a number: Testing divisors from 2 to {n-1}...")
-    for i in range(2, n-1):
-        n % 1 == 0 # checking to see when a number between 2 to n-1 has a remainder and if it does n is prime
-        print(f"{n} is not prime (divisible by 3)")
-        break # if n is prime the loop will stop right here but if not it will drop to the else statement
+    for i in range(2, n): #I had ai help me debug the range for loop because it would print 17 is not prime
+        if n % i == 0: # checking to see when a number between 2 to n-1 has a remainder and if it does n is prime
+            print(f"{n} is not prime (divisible by 3)")
+            break # if n is prime the loop will stop right here but if not it will drop to the else statement
     else:
         print(f"{n} is prime!")
 print()
@@ -49,4 +51,4 @@ for row in range(1,11):
     for col in range(1,11):
         product= row * col
         print(f"{product:4}", end="") # Prints the products of row * coulumn 
-    print() 
+    print()
