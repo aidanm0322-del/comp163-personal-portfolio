225cb7b36ef2e7c16be06886c414ea97bad5c066
aidanm0322-del <aidanm0322@gmail.com>
2025-10-09 20:48:10 -0400

    Add name and address with string methods

--- comp-163-assignment6.py
+++ comp-163-assignment6.py
@@ -7,4 +7,24 @@ name=contact_1.split("|")[0]
 name=name.title()
 name=name.strip()
 phone=contact_1.split("|")[1]
- 
+digits_only = ""
+address=address.strip()
+address=address.title()
+address=address.replace("Nc","NC")
+
+#Contact 2 Directory 
+
+
+
+
+
+
+
+
+print("Enter contact information (format: name|phone|email|address):")
+print()
+print("=== CONTACT DIRECTORY ===")
+print()
+print("CONTACT 1:")
+print(f"Name:     {name}")
+print(f"Address:  {address}")
