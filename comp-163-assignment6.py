206c9502a2f6edcef0dec5c819532599f732ab5c
aidanm0322-del <aidanm0322@gmail.com>
2025-10-10 16:03:32 -0400

    Add name and address cleaning with string methods

--- comp-163-assignment6.py
+++ comp-163-assignment6.py
@@ -1,5 +1,4 @@
 
-
 print("Enter contact information (format: name|phone|email|address):")
 print()
 print("=== CONTACT DIRECTORY ===")
@@ -17,7 +16,25 @@ while True:
         break
 
     contact_count += 1
-    parts = contact.split("|") 
+    parts = contact.split("|")
     
+    #name format
+    name = parts[0]
+    name = name.strip()
+    name = name.title()
+    
+    name_parts=name.split()
+    if len(name_parts) >= 2:
+        first=" ".join(name_parts[:-1])
+        last=name_parts[-1]
+        last_first = f"{last},{first}"
+    else:
+        last_first=name
 
-   
+    #Address format 
+    address = parts[3]
+    address = address.strip()
+    address = address.title()
+    address = address.replace("Nc", "NC")
+
+
